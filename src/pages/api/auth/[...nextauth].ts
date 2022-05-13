8b8f64050606b04c5f6d8e63da5ca99c727eecfa
Tom Nonato Junior <77701041+tomnjunior@users.noreply.github.com>
2022-05-13 15:45:17 -0300

    Em progresso

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,17 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
 
 export default NextAuth({
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            scope: 'read:user'
+            authorization: {
+                params: {
+                    scope: "read:user",
+                },
+            },
         }),
     ],
-})
+});
